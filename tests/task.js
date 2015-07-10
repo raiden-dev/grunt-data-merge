1f4cfa63f0c98731d820962a8fce4ccebcbb60de
Andrey Kravtsov <raidendev@gmail.com>
2015-07-10 13:01:59 +0300

    Refactor old and add new tests

--- tests/task.js
+++ tests/task.js
@@ -1,5 +1,6 @@
 var grunt = require('grunt'),
     yaml = require('js-yaml'),
+    should = require('should'),
     hooker = grunt.util.hooker,
     dataMergeTask = require('../tasks/data-merge');
 
@@ -24,40 +25,93 @@ describe('Grunt task', function () {
     hooker.unhook(grunt.log);
   });
 
-  it('should exist', function () {
-    if (!grunt.task.exists('data-merge')) {
-      throw new Error('Task "data-merge" was not found');
-    }
-  });
-
-  it('should write result as YAML for .yml|.yaml dests', function () {
-    var src = 'tmp/foo.yml',
-        dest = 'tmp/bar.yaml';
-
-    hooker.hook(grunt.file, 'write', function (dest, data) {
-      try {
-        yaml.safeLoad(data);
-      }
-      catch (err) {
-        err.message = 'Writing not valid YAML data. ' + err.message;
-        throw err;
-      }
-    });
-
+  function runTask(src, dest) {
     task.call({
       options: function () {
         return {};
       },
 
       files: [{
-        src: [src],
+        src: Array.isArray(src) ? src : [src],
         dest: dest
       }]
     });
+  }
+
+  it('should be registered', function () {
+    grunt.task.exists('data-merge').should.be.true();
   });
 
-  it('should write result as JSON for the rest', function () {
-    var src = 'tmp/foo.json',
+  it('should merge JSONs', function () {
+    var json1 = '{"foo":{"bar":24,"baz":null}}';
+    var json2 = '{"foo":{"bar":42}}';
+
+    var expected = '{"foo":{"bar":42,"baz":null}}';
+
+    var count = 0;
+    hooker.hook(grunt.file, 'read', function () {
+      count++;
+      return hooker.preempt(count <= 1 ? json1 : json2);
+    });
+
+    hooker.hook(grunt.file, 'write', function (dest, data) {
+      data.should.be.equal(expected);
+    });
+
+    runTask(['foo', 'bar']);
+  });
+
+  it('should merge YAMLs', function () {
+    var yaml1 = 'foo:\n  bar: 24\n  baz: null\n';
+    var yaml2 = 'foo:\n  bar: 42\n';
+
+    var expected = 'foo:\n  bar: 42\n  baz: null\n';
+
+    var count = 0;
+    hooker.hook(grunt.file, 'read', function () {
+      count++;
+      return hooker.preempt(count <= 1 ? yaml1 : yaml2);
+    });
+
+    hooker.hook(grunt.file, 'write', function (dest, data) {
+      data.should.be.equal(expected);
+    });
+
+    runTask(['foo.yml', 'bar.yaml'], 'out.yaml');
+  });
+
+  it('should merge JSON with YAML', function () {
+    var json = '{"foo":{"bar":24,"baz":null}}';
+    var yaml = 'foo:\n  bar: 42\n';
+
+    var expected = '{"foo":{"bar":42,"baz":null}}';
+
+    var count = 0;
+    hooker.hook(grunt.file, 'read', function () {
+      count++;
+      return hooker.preempt(count <= 1 ? json : yaml);
+    });
+
+    hooker.hook(grunt.file, 'write', function (dest, data) {
+      data.should.be.equal(expected);
+    });
+
+    runTask(['foo.json', 'bar.yaml']);
+  });
+
+  it('should write result as YAML', function () {
+    var src = 'tmp/foo.yml',
+        dest = 'tmp/bar.yaml';
+
+    hooker.hook(grunt.file, 'write', function (dest, data) {
+      yaml.safeLoad(data).should.not.throw();
+    });
+
+    runTask(src, dest);
+  });
+
+  it('should write result as JSON', function () {
+    var src = 'tmp/foo',
         dest = 'tmp/bar.json';
 
     hooker.hook(grunt.file, 'read', function () {
@@ -65,25 +119,10 @@ describe('Grunt task', function () {
     });
 
     hooker.hook(grunt.file, 'write', function (dest, data) {
-      try {
-        JSON.parse(data);
-      }
-      catch (err) {
-        err.message = 'Writing not valid JSON data. ' + err.message;
-        throw err;
-      }
+      JSON.parse(data).should.not.throw();
     });
 
-    task.call({
-      options: function () {
-        return {};
-      },
-
-      files: [{
-        src: [src],
-        dest: dest
-      }]
-    });
+    runTask(src, dest);
   });
 
 });
